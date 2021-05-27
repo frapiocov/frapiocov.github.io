91c44a0905daf5bcc02801f76a61787866f3e5d8
Francesco Pio Covino <frapiocov@users.noreply.github.com>
2021-05-27 21:07:03 +0200

    add modal

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
-function wait(text) {
-    document.getElementById("avviso").innerHTML= "Wait! " +text+ " account will arrive soon.";
+var phrase;
+
+var modal = document.getElementById("myModal");
+var modalText = document.getElementById("modalText");
+var span = document.getElementsByClassName("close")[0];
+
+function openModal(text) {
+  phrase = 'Ops... ' + text + ' is not ready yet!';  
+  modal.style.display = "block";
+  modalText.innerHTML = "<span style='font-size:45px;'>&#128546;</span>" + phrase + ' Will arrive soon, i promise.';
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
